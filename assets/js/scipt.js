30cd9760d6ff4ff24c0979c3fa92b5e9973467ae
keith-mikel <keith.mikel@e5sol.com>
2023-05-22 11:50:02 -0500

    setup and psuedo code

---
+++ assets/js/scipt.js
@@ -0,0 +1,15 @@
+// on click of start quiz start timer for 75 seconds
+
+// write out all questions and their answers in an array item 0 being the question and 1-4 answer
+// item 0 will be the OL item and the 4 answers will be the lis items
+
+//on selection of an answer it will determine if that answer was correct or not
+    // if correct quiz will move to next question, and display correct under quiz
+    // if incorrect the quiz will deduct 10 seconds and display incorrect
+
+// if the timer reaches 0 before the quiz is finished, it will bring you to a try again screen
+
+// if the quiz is completed the score will be calculated based on the remaining time
+     // high score form will be filled and stored in the high score list sorted by high to low score
+     // play again button will restart quiz
+     //clear high scores button will clear stored high scores
