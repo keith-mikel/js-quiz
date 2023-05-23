1f0a35c8ae98af238e14990afaf72c4903347ab6
keith-mikel <keith.mikel@e5sol.com>
2023-05-23 16:44:03 -0500

    created html, and structure for timer with question

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,5 +1,66 @@
 // on click of start quiz start timer for 75 seconds
 
+var TimerEl = document.querySelector('#timer')
+var startButton = document.querySelector('#startButton')
+var questionEl = document.querySelector('#question')
+var option1El = document.querySelector('#option1')
+var option2El = document.querySelector('#option2')
+var option3El = document.querySelector('#option3')
+var option4El = document.querySelector('#option4')
+
+var time = 75;
+TimerEl.textContent = time
+questionEl.textContent = 'Click Start Quiz'
+
+
+var q1 = {
+question: "What is Javascript?",
+answers: [
+  { answerText: "A book about Coffee", isCorrect: false },
+  { answerText: "A coding Language", isCorrect: true },
+  { answerText: "A speaking language", isCorrect: false },
+  { answerText: "A waste of time", isCorrect: false }
+]
+};
+console.log(q1.answers[0].answerText)
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      time--;
+      TimerEl.textContent = time;
+  
+      if(timerSet === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+  }
+
+  function startQuiz() {
+    questionEl.textContent = q1.question;
+    option1El.textContent= q1.answers[0].answerText;
+    option2El.textContent= q1.answers[1].answerText;
+    option3El.textContent= q1.answers[2].answerText;
+    option4El.textContent= q1.answers[3].answerText;
+
+    option1El.addEventListener('click', function() {
+
+    }
+    )
+  }
+
+
+  startButton.addEventListener('click', function() {
+    setTime();
+    startQuiz();
+  } 
+  )
+
+
+
+
 // write out all questions and their answers in an array item 0 being the question and 1-4 answer
 // item 0 will be the OL item and the 4 answers will be the lis items
 
