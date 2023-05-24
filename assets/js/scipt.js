196f4a1a77836df47a32a705cc535e7c74cb5f79
keith-mikel <keith.mikel@e5sol.com>
2023-05-24 17:07:32 -0500

    added questions and logic to progress, as well as deduct time and show if answer was correct

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -7,50 +7,108 @@ var option1El = document.querySelector('#option1')
 var option2El = document.querySelector('#option2')
 var option3El = document.querySelector('#option3')
 var option4El = document.querySelector('#option4')
+var answerButton = document.querySelector('.answerButton')
+var solution = document.querySelector('#solution')
 
 var time = 75;
 TimerEl.textContent = time
 questionEl.textContent = 'Click Start Quiz'
 
 
-var q1 = {
-question: "What is Javascript?",
-answers: [
-  { answerText: "A book about Coffee", isCorrect: false },
-  { answerText: "A coding Language", isCorrect: true },
-  { answerText: "A speaking language", isCorrect: false },
-  { answerText: "A waste of time", isCorrect: false }
+var quiz = {
+  quizQuestions:[
+  {
+    question: "What is Javascript?",
+    answers: [
+      { answerText: "A book about Coffee", isCorrect: false },
+      { answerText: "A coding Language", isCorrect: true },
+      { answerText: "A speaking language", isCorrect: false },
+      { answerText: "A waste of time", isCorrect: false }
+    ]
+  },
+  {
+    question: "What can querySelector be used for?",
+    answers: [
+      { answerText: "Select multiple elements that match a CSS selector.", isCorrect: false },
+      { answerText: "Change the content of an HTML element.", isCorrect: false },
+      { answerText: "Retrieve the value of a form input field.", isCorrect: false },
+      { answerText: "Select the first element that matches a CSS selector.", isCorrect: true }
+    ]
+  },
+  {
+    question: "What is the purpose of the setTimeout function in JavaScript??",
+    answers: [
+      { answerText: "Execute a function repeatedly at a specified interval.", isCorrect: false },
+      { answerText: "Stop the execution of a function.", isCorrect: false },
+      { answerText: "Delay the execution of a function by a specified amount of time.", isCorrect: true },
+      { answerText: "Bind a function to a specific event.", isCorrect: false }
+    ]
+  },
+  {
+    question: "What does the NaN value represent in JavaScript?",
+    answers: [
+      { answerText: "Not a Number' - indicating an invalid or unrepresentable numeric value.", isCorrect: true },
+      { answerText: "'Not a Null' - indicating the absence of a null value.", isCorrect: false },
+      { answerText: "'Not a Name' - indicating an undefined or missing variable name.", isCorrect: false },
+      { answerText: "'Not an Object' - indicating a variable that does not refer to an object.", isCorrect: false }
+    ]
+  },
 ]
-};
-console.log(q1.answers[0].answerText)
+}
+   
 
-function setTime() {
+function startQuiz() {
+  var Q = 0;
+
+  function updateQuestion() {
+    questionEl.textContent = quiz.quizQuestions[Q].question;
+
+    option1El.textContent = quiz.quizQuestions[Q].answers[0].answerText;
+    option1El.setAttribute('data-isCorrect', quiz.quizQuestions[Q].answers[0].isCorrect);
+
+    option2El.textContent = quiz.quizQuestions[Q].answers[1].answerText;
+    option2El.setAttribute('data-isCorrect', quiz.quizQuestions[Q].answers[1].isCorrect);
+
+    option3El.textContent = quiz.quizQuestions[Q].answers[2].answerText;
+    option3El.setAttribute('data-isCorrect', quiz.quizQuestions[Q].answers[2].isCorrect);
+
+    option4El.textContent = quiz.quizQuestions[Q].answers[3].answerText;
+    option4El.setAttribute('data-isCorrect', quiz.quizQuestions[Q].answers[3].isCorrect);
+  }
+
+  function onNextQuestion(event) {
+    console.log(event.target.textContent)
+    console.log(event.target.getAttribute('data-isCorrect'))
+    if (event.target.getAttribute('data-isCorrect') == 'true') {
+      solution.textContent = 'Correct!'
+      
+    } else {
+      solution.textContent = 'Wrong!'
+      time -= 10;
+    }
+    Q++;
+    updateQuestion();
+  }
+
+  updateQuestion();
+
+  answerButton.addEventListener('click', onNextQuestion);
+}
+
+
+  function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function() {
       time--;
       TimerEl.textContent = time;
   
-      if(timerSet === 0) {
+      if(time === 0) {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
       }
   
     }, 1000);
-  }
-
-  function startQuiz() {
-    questionEl.textContent = q1.question;
-    option1El.textContent= q1.answers[0].answerText;
-    option2El.textContent= q1.answers[1].answerText;
-    option3El.textContent= q1.answers[2].answerText;
-    option4El.textContent= q1.answers[3].answerText;
-
-    option1El.addEventListener('click', function() {
-
-    }
-    )
-  }
-
+  };
 
   startButton.addEventListener('click', function() {
     setTime();
